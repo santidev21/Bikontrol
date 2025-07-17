b679182340f60062d630de7ba7870711e768ea70
Santidev21 <orsantiago21@gmail.com>
2025-07-16 22:43:56 -0500

    chore(structure): add base modules for layout, dashboard, and motorcycles.

---
+++ bikontrol-web/src/app/modules/motorcycles/motorcycles.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MotorcyclesRoutingModule } from './motorcycles-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MotorcyclesRoutingModule
+  ]
+})
+export class MotorcyclesModule { }
