ed2f1f6854863946667f13962484eb39498d4888
santidev21 <orsantiago21@gmail.com>
2026-05-29 12:23:46 -0500

    Added alias for environment.

--- bikontrol-web/src/app/modules/auth/services/auth.service.ts
+++ bikontrol-web/src/app/modules/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
-import { environment } from '../../../../environments/environment';
+import { environment } from '@env/environment';
 import { LoginResponse, RegisterResponse } from '../interfaces/auth.model';
 
 @Injectable({
