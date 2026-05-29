c0c04b865510d73429bde44af882ac2e451c4ffd
santidev21 <orsantiago21@gmail.com>
2026-05-29 12:20:20 -0500

    Edited Jest config.

--- bikontrol-web/jest.config.js
+++ bikontrol-web/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^(?:\\.\\./){4}environments\\/environment$': '<rootDir>/src/environments/environment.ts',
+    '^.+\\/environments\\/environment$': '<rootDir>/src/environments/environment.ts',
     '\\.(scss|css|svg|png|jpg)$': '<rootDir>/__mocks__/styleMock.js'
   },
   transform: {
